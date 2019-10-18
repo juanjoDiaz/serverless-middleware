1aad7b36af94737a21ac586a9bf91da3baf7f6cd
Juanjo Diaz <juanjo.diazmo@gmail.com>
2019-10-18 15:43:41 +0200

    Skip all invalid characters from module name

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,7 @@
-
 function parseHandler(handler) {
   const [module, fn] = handler.split(/\.(?=[^.]+$)/);
   return {
-    name: module.replace(/\s|\//g, '_'),
+    name: module.replace(/^[^a-zA-Z_$]|[^\w_$]/g, '_'),
     module,
     fn,
   };
